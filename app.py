50abc8afe104be72cce2ac00938cc78b69a70873
hitheshp1 <hithesh.patel@workday.com>
2025-08-11 18:44:54 +0530

    updated files

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import streamlit as st
 
-# Connection
-try:
-    from snowflake.snowpark.context import get_active_session
-    session = get_active_session()
-except:
-    import toml
-    from snowflake.snowpark.session import Session
+def main():
+    with st.sidebar:
+        st.title("Main menu ")
+        st.write("Home")
+    
 
-    snowflake_connection_cfg = toml.load('connection.toml')['snowflake']
-    session = Session.builder.configs(snowflake_connection_cfg).create()
-
-# Contents
-st.title("Harato Page")
-
-query = "SELECT id as test FROM D_HARATO_DB.DEV.NEW_TABLE"
-data = session.sql(query).collect()
-st.dataframe(data)
+if __name__=='__main__':
+    main()
